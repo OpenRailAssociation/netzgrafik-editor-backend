654650399a735e3ced29eccc65a877b48715a6da
Adrian Egli <adrian.egli@sbb.ch>
2026-03-26 10:54:56 +0100

    fix: add new extra comment line

--- src/main/java/ch/sbb/pfi/netzgrafikeditor/api/project/ProjectService.java
+++ src/main/java/ch/sbb/pfi/netzgrafikeditor/api/project/ProjectService.java
@@ -201,7 +201,7 @@ public class ProjectService {
                                                                                         .authenticationService
                                                                                         .getCurrentSubjectId()
                                                                                         .getValue()))));
-
+        // ----------------------------------------------------------------------------------------------------
         return this.context
                 .selectFrom(PROJECTS)
                 .whereExists(filterCondition)
